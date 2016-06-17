47a705e6b82e1695683264d750dee0223fd7db5b
Shawon Ashraf <shawon13@live.com>
2016-06-18 00:31:00 +0600

    BOO

---
+++ Sorting/Quick Sort/Quick Sort/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quick_Sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { -45, 100, 2, 89, -1, 7, 54, -3 };
+
+            
+
+            Console.WriteLine("\nPress any key to exit .....");
+            Console.ReadLine();
+        }
+    }
+}

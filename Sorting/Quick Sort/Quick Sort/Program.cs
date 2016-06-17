d85100d5b69b2e8aa72cb5f996aba191737092ee
Shawon Ashraf <shawon13@live.com>
2016-06-18 03:06:46 +0600

    Eureka!

--- Sorting/Quick Sort/Quick Sort/Program.cs
+++ Sorting/Quick Sort/Quick Sort/Program.cs
@@ -12,10 +12,60 @@ namespace Quick_Sort
         {
             int[] arr = { -45, 100, 2, 89, -1, 7, 54, -3 };
 
-            
+            QuickSort(arr, 0, arr.Length - 1);
 
             Console.WriteLine("\nPress any key to exit .....");
             Console.ReadLine();
+        }
+
+        static int Partition(int[] arr, int p, int r)
+        {
+            int i =  p - 1;
+            int x = arr[r];
+
+            for(int j = p; j < r; j++)
+            {
+                if (arr[j] <= x)
+                {
+                    i++;
+
+                    // swap
+                    arr[j] = Swap(arr[i], arr[i] = arr[j]);
+                }
+            }
+            arr[r] = Swap(arr[i + 1], arr[i + 1] = arr[r]);
+
+            return i + 1;
+        }
+
+        static void QuickSort(int[] arr, int p, int r)
+        {
+            if(p < r)
+            {
+                int q = Partition(arr, p, r);
+                QuickSort(arr, p, q - 1);
+                QuickSort(arr, q + 1, r);
+            }
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("Call() returned  : ");
+
+            Console.ForegroundColor = ConsoleColor.White;
+            PrintArray(arr);
+            
+        }
+
+        static int Swap(int x, int y)
+        {
+            return x;
+        }
+
+        static void PrintArray(int[] a)
+        {
+            foreach (var i in a)
+            {
+                Console.Write(i + "  ");
+            }
+            Console.WriteLine();
         }
     }
 }
